d7eb8b73e8c39fec3116a664ff6ce53e14abcd4d
hinohara <hinohara@hinoharanoMacBook-Air.local>
2020-06-15 07:57:07 +0900

    add props

--- react_and_redux/src/App.js
+++ react_and_redux/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
 function App() {
+  const profiles = [
+    {name: "Taro"},
+    {name: "Haneko"},
+    {},
+  ]
   return (
     <div>
-      <Cat />
+      {/* props */}
+      {
+        profiles.map((profile, index) => {
+        //{*/ keyが必要 */}
+          return <Cat name={profile.name} key={index}/>
+        })
+      }
     </div>
   );
 }
 
-
-const Cat = () =>{
-  return <div>Meow</div>
+//関数コンポーネント
+const Cat = (props) =>{
+  return <div>Meow. I am {props.name}.</div>
 }
 
+Cat.defaultProps = {
+  name: "Noname"
+}
 export default App;
