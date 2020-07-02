f71989ff67a3ec7c64e11dd4c0be4732ef9538d6
hinohara <hinoshin0817@gmail.com>
2020-07-02 10:11:58 +0900

    add eventsshow

--- react_and_redux/src/actions/index.js
+++ react_and_redux/src/actions/index.js
@@ -15,6 +15,7 @@ export const decrement = () => ({
 */
 export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENT = 'CREATE_EVENT'
+export const DELETE_EVENT = 'DELETE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -29,4 +30,9 @@ export const postEvent = values => async dispatch => {
   const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
     dispatch({ type: CREATE_EVENT, response
   })
+}
+
+export const deleteEvent = id => async dispatch => {
+  await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+  dispatch({ type: DELETE_EVENT, id})
 }
