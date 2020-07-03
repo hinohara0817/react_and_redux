e91ca1479ca0e668e7cb968a17406a8a88139c61
hinohara <hinoshin0817@gmail.com>
2020-07-03 10:01:12 +0900

    add putevent

--- react_and_redux/src/reducers/events.js
+++ react_and_redux/src/reducers/events.js
@@ -14,12 +14,17 @@ export default(state = initialState, action) => {
 }
 */
 import _ from 'lodash'
-import{ READ_EVENTS, DELETE_EVENT } from '../actions'
+import{ CREATE_EVENT, READ_EVENTS, READ_EVENT, UPDATE_EVENT, DELETE_EVENT } from '../actions'
 
 export default (events = {}, action) => {
   switch(action.type){
+    case CREATE_EVENT:
     case READ_EVENTS:
+    case UPDATE_EVENT:
       return _.mapKeys(action.response.data, 'id')
+    case READ_EVENT:
+      const data = action.response.data
+      return { ...events, [data.id]: data}
     case DELETE_EVENT:
       delete events[action.id]
       return { ...events }
