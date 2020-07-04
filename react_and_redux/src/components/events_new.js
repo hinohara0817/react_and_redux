fbc870432e196493aa8c5ad30daf4a8f18e19a5f
hinohara <hinoshin0817@gmail.com>
2020-07-04 09:21:31 +0900

    apply material-ui

--- react_and_redux/src/components/events_new.js
+++ react_and_redux/src/components/events_new.js
@@ -1,46 +1,50 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
-import{ Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
-import {postEvent} from '../actions'
+import { postEvent } from '../actions'
 
 class EventsNew extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
   }
-  renderField(field){
+
+  renderField(field) {
     const { input, label, type, meta: { touched, error } } = field
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
-  async onSubmit(values){
+
+  async onSubmit(values) {
     await this.props.postEvent(values)
     this.props.history.push('/')
   }
-  render(){
-    const { handleSubmit, pristine, submitting, invalid} =this.props
-    return(
-      <React.Fragment>
-        <form onSubmit={handleSubmit(this.onSubmit)}>
-          <div>
-          <Field label="Title" name="title" type="text" component={this.renderField} />
-          </div>
-          <div>
-          <Field label="Body" name="body" type="text" component={this.renderField} />
-          </div>
-          <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-            <Link to="/" >Cancel</Link>
-          </div>
-        </form>
-      </React.Fragment>
+
+  render() {
+    const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12 }
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+
+        <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+        <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />}/>
+      </form>
     )
   }
 }
@@ -48,13 +52,14 @@ class EventsNew extends Component {
 const validate = values => {
   const errors = {}
 
-  if(!values.title) errors.title = "Enter a title, please"
-  if(!values.body) errors.body = "Enter a body, please"
+  if (!values.title) errors.title = "Enter a title, please."
+  if (!values.body) errors.body = "Enter a body, please."
+
   return errors
 }
 
 const mapDispatchToProps = ({ postEvent })
-export default connect(null, mapDispatchToProps)(
-  reduxForm({validate, form: 'eventNewForm'})(EventsNew)
-)
 
+export default connect(null, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
